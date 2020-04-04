1448cca59dd55120c24d45c87e758a0929bd2afe
FrostiaKk <60479225+FrostiaKk@users.noreply.github.com>
2020-04-05 00:51:25 +0200

    UI
    Created UI for drawing

---
+++ DigitsRecogniton/DigitsRecogniton/ViewModels/InkStrokeCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Ink;
+
+namespace DigitsRecogniton.ViewModels
+{
+    class InkStrokeCollection : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
---
+++ DigitsRecogniton/DigitsRecogniton/ViewModels/ShellViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.IO;
+
+namespace DigitsRecogniton.ViewModels
+{
+	public class ShellViewModel : Screen
+	{
+		private string _whatDigit;
+
+		public string WhatDigit
+		{
+			get { return _whatDigit; }
+			set { _whatDigit = value; }
+		}
+
+		private ICommand _saveCanvasCommand;
+
+		public ICommand SaveCanvasCommand
+		{
+			get
+			{
+				if (_saveCanvasCommand == null)
+					_saveCanvasCommand = new SaveCanvas();
+				return _saveCanvasCommand;
+			}
+			set { _saveCanvasCommand = value; }
+		}
+		class SaveCanvas : ICommand
+		{
+			#region ICommand Members  
+
+			public bool CanExecute(object parameter)
+			{
+				return true;
+			}
+			public event EventHandler CanExecuteChanged
+			{
+				add { CommandManager.RequerySuggested += value; }
+				remove { CommandManager.RequerySuggested -= value; }
+			}
+
+			public void Execute(object parameter)
+			{
+				var size = new Size(100, 140);
+				((UIElement)parameter).Measure(size);
+				((UIElement)parameter).Arrange(new Rect(size));
+				RenderTargetBitmap rtb = new RenderTargetBitmap(100, 140, 96d, 96d, PixelFormats.Default);
+				rtb.Render((UIElement)parameter);
+				BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+				encoder.Frames.Add(BitmapFrame.Create(rtb));
+				FileStream fs = File.Open(@"d:\test.bmp", FileMode.Create);
+				encoder.Save(fs);
+				fs.Close();
+			}
+			#endregion
+
+
+		}
+	}
+}

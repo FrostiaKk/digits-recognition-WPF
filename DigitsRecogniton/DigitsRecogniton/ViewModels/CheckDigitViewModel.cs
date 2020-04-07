52e21b0f73590fda39bfa1a8dafe4f0fc0958493
FrostiaKk <60479225+FrostiaKk@users.noreply.github.com>
2020-04-07 19:30:18 +0200

    Added ChildViews

---
+++ DigitsRecogniton/DigitsRecogniton/ViewModels/CheckDigitViewModel.cs
@@ -0,0 +1,99 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DigitsRecogniton.ViewModels
+{
+    class CheckDigitViewModel : Screen
+    {
+		private ICommand _saveCanvasCommand;
+
+		public ICommand SaveCanvasCommand
+		{
+			get
+			{
+				if (_saveCanvasCommand == null)
+					_saveCanvasCommand = new SaveCanvas();
+				return _saveCanvasCommand;
+			}
+			set { _saveCanvasCommand = value; }
+		}
+		class SaveCanvas : ICommand
+		{
+			#region ICommand Members  
+
+			public bool CanExecute(object parameter)
+			{
+				return true;
+			}
+			public event EventHandler CanExecuteChanged
+			{
+				add { CommandManager.RequerySuggested += value; }
+				remove { CommandManager.RequerySuggested -= value; }
+			}
+
+			public void Execute(object parameter)
+			{
+				var size = new Size(100, 140);
+				((UIElement)parameter).Measure(size);
+				((UIElement)parameter).Arrange(new Rect(size));
+				RenderTargetBitmap rtb = new RenderTargetBitmap(100, 140, 96d, 96d, PixelFormats.Default);
+				rtb.Render((UIElement)parameter);
+				BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+				encoder.Frames.Add(BitmapFrame.Create(rtb));
+				FileStream fs = File.Open(@"d:\test.bmp", FileMode.Create);
+				encoder.Save(fs);
+				fs.Close();
+			}
+			#endregion
+
+
+		}
+
+		private ICommand _clearCanvasCommand;
+
+		public ICommand ClearCanvasCommand
+		{
+			get
+			{
+				if (_clearCanvasCommand == null)
+					_clearCanvasCommand = new ClearCanvas();
+				return _clearCanvasCommand;
+			}
+			set { _clearCanvasCommand = value; }
+		}
+		class ClearCanvas : ICommand
+		{
+			#region ICommand Members  
+
+			public bool CanExecute(object parameter)
+			{
+				return true;
+			}
+			public event EventHandler CanExecuteChanged
+			{
+				add { CommandManager.RequerySuggested += value; }
+				remove { CommandManager.RequerySuggested -= value; }
+			}
+
+			public void Execute(object parameter)
+			{
+				InkCanvas newCanvas = (InkCanvas)parameter;
+				newCanvas.Strokes.Clear();
+				//((UIElement)parameter).
+			}
+			#endregion
+
+
+		}
+	}
+}
--- DigitsRecogniton/DigitsRecogniton/ViewModels/ShellViewModel.cs
+++ DigitsRecogniton/DigitsRecogniton/ViewModels/ShellViewModel.cs
@@ -10,10 +10,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media;
 using System.IO;
 using System.Windows.Controls;
+using DigitsRecogniton.Views;
 
 namespace DigitsRecogniton.ViewModels
 {
-	public class ShellViewModel : Screen
+	public class ShellViewModel : Conductor<object>
 	{
 		private string _whatDigit;
 
@@ -23,85 +24,11 @@ namespace DigitsRecogniton.ViewModels
 			set { _whatDigit = value; }
 		}
 
-		private ICommand _saveCanvasCommand;
+		
 
-		public ICommand SaveCanvasCommand
+		public void LoadCheckDigit()
 		{
-			get
-			{
-				if (_saveCanvasCommand == null)
-					_saveCanvasCommand = new SaveCanvas();
-				return _saveCanvasCommand;
-			}
-			set { _saveCanvasCommand = value; }
-		}
-		class SaveCanvas : ICommand
-		{
-			#region ICommand Members  
-
-			public bool CanExecute(object parameter)
-			{
-				return true;
-			}
-			public event EventHandler CanExecuteChanged
-			{
-				add { CommandManager.RequerySuggested += value; }
-				remove { CommandManager.RequerySuggested -= value; }
-			}
-
-			public void Execute(object parameter)
-			{
-				var size = new Size(100, 140);
-				((UIElement)parameter).Measure(size);
-				((UIElement)parameter).Arrange(new Rect(size));
-				RenderTargetBitmap rtb = new RenderTargetBitmap(100, 140, 96d, 96d, PixelFormats.Default);
-				rtb.Render((UIElement)parameter);
-				BmpBitmapEncoder encoder = new BmpBitmapEncoder();
-				encoder.Frames.Add(BitmapFrame.Create(rtb));
-				FileStream fs = File.Open(@"d:\test.bmp", FileMode.Create);
-				encoder.Save(fs);
-				fs.Close();
-			}
-			#endregion
-
-
-		}
-
-		private ICommand _clearCanvasCommand;
-
-		public ICommand ClearCanvasCommand
-		{
-			get
-			{
-				if (_clearCanvasCommand == null)
-					_clearCanvasCommand = new ClearCanvas();
-				return _clearCanvasCommand;
-			}
-			set { _clearCanvasCommand = value; }
-		}
-		class ClearCanvas : ICommand
-		{
-			#region ICommand Members  
-
-			public bool CanExecute(object parameter)
-			{
-				return true;
-			}
-			public event EventHandler CanExecuteChanged
-			{
-				add { CommandManager.RequerySuggested += value; }
-				remove { CommandManager.RequerySuggested -= value; }
-			}
-
-			public void Execute(object parameter)
-			{
-				InkCanvas newCanvas = (InkCanvas)parameter;
-				newCanvas.Strokes.Clear();
-				//((UIElement)parameter).
-			}
-			#endregion
-
-
+			ActivateItem(new CheckDigitViewModel());
 		}
 
 
